88e17555b4b547b3d7136e6abad02564c4e1595f
Sanket Pathak <sanketpathak2005@gmail.com>
2025-03-03 08:53:41 +0530

    Parallax component added in app.js(Main)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
+import Parallax from "./components/parallax";
 import { GlobalStyle } from "./globalStyles";
 import { lazy, Suspense } from "react";
-
+import { useState, useEffect } from "react";
+import "./App.css"
 const Home = lazy(() => import("./Pages/Home"));
 const Header = lazy(() => import("./components/Header/index"));
 const Footer = lazy(() => import("./components/Footer/index"));
 const ScrollToTop = lazy(() => import("./components/ScrollToTop/index"));
 
 function App() {
+  const [showParallax, setShowParallax] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowParallax(false); // Hide Parallax after 5 seconds
+    }, 5000);
+
+    return () => clearTimeout(timer); // Cleanup the timer
+  }, []);
+
   return (
     <>
-      <Suspense fallback={null}>
-        <GlobalStyle />
-        {/* Hi There! */}
-        <ScrollToTop />
-        <Header />
-        <Home />
-        <Footer />
-      </Suspense>
+      {showParallax && (
+        <div className="parallax-wrapper">
+          <Parallax />
+        </div>
+      )}
+      {!showParallax && (
+        <Suspense fallback={null}>
+          <GlobalStyle />
+          <ScrollToTop />
+          <Header />
+          <Home />
+          <Footer />
+        </Suspense>
+      )}
     </>
   );
 }
-
 export default App;
