aaebccbe35250936b6e7bc83b1602c3702b16ca2
Sanket Pathak <sanketpathak2005@gmail.com>
2025-03-14 21:14:17 +0530

    Dummy value removed

--- src/Sections/Testimonials/index.js
+++ src/Sections/Testimonials/index.js
@@ -91,21 +91,10 @@ const Testimonials = () => {
 
           <Card
             text="Das with an extra s!"
-            name="Ankita"
+            name="Ankita Dass"
             image="avatar-2"
           />
 
-          <Card
-            text="CodeBucks has been essential part of our business. I would definetly recommend CodeBucks. It has been amazing to have them."
-            name="Jenny (CodeCall)"
-            image="avatar-3"
-          />
-
-          <Card
-            text="CodeBucks has been essential part of our business. I would definetly recommend CodeBucks. It has been amazing to have them."
-            name="Jenny (CodeCall)"
-            image="avatar-4"
-          />
         </Slider>
       </Carousal>
     </Section>
