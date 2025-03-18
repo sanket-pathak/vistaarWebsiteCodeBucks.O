ccd4e2f27cf424310538841804589a98aefd9e3c
Sanket Pathak <sanketpathak2005@gmail.com>
2025-03-18 17:13:45 +0530

    Animation Added+Logo responsive

--- src/Sections/Hero/index.js
+++ src/Sections/Hero/index.js
@@ -1,23 +1,10 @@
-// This is HeroSection component, Main top section of website
-
-import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import { useEffect } from "react";
 import styled, { keyframes } from "styled-components";
-import pinkBlob from "../../assets/blobPink.png";
-import purpleBlob from "../../assets/blob purple.png";
-import whiteBlob from "../../assets/blob white.png";
-import arrow from "../../assets/Arrow Right.svg";
 import bgImage from "../../assets/landingBG4.jpg";
-import funnySVG from "../../assets/funnySVG.svg";
-import './index.css';
 import vistaar from "../../assets/Vistaar.svg";
-import { useEffect, useRef } from "react";
-import { gsap } from "gsap";
-
-const move = keyframes`
-0% { transform: translateY(-5px)  }
-    50% { transform: translateY(10px) }
-    100% { transform: translateY(-5px) }
-`;
+import vistaarOutline from "../../assets/vistaarOutline.svg";
+import Vivus from "vivus";
+import "./index.css";
 
 const HomeSection = styled.section`
   width: 100vw;
@@ -25,206 +12,68 @@ const HomeSection = styled.section`
   background-color: #0a0b10;
   display: flex;
   justify-content: center;
+  align-items: center;
   position: relative;
-  @media only Screen and (max-width: 48em) {
+  flex-direction: column;
+  @media only screen and (max-width: 48em) {
     height: 70vw;
     display: block;
   }
-  @media only Screen and (max-width: 420px) {
+  @media only screen and (max-width: 420px) {
     height: auto;
     padding-bottom: 2rem;
   }
 `;
-
-const Blobs = styled.div`
-  width: 100%;
-  position: absolute;
-  right: 0;
-  @media only Screen and (max-width: 48em) {
-    opacity: 0.5;
-  }
-`;
-
-// const PinkBlob = styled.div`
-//   width: calc(15% + 15vw);
-//   position: absolute;
-//   right: 0;
-//   top: calc(5rem + 5vw);
-//   z-index: 6;
-// `;
-// const PurpleBlob = styled.div`
-//   width: calc(10% + 10vw);
-//   position: absolute;
-//   right: 0;
-// `;
-// const WhiteBlob = styled.div`
-//   width: calc(20% + 20vw);
-//   position: absolute;
-//   right: calc(3.5rem + 3.5vw);
-//   top: calc(2rem + 2vw);
-//   z-index: 5;
-// `;
 
 const MainContent = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   width: 70vw;
-  @media only Screen and (max-width: 48em) {
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  flex-direction: column;
+  @media only screen and (max-width: 48em) {
     width: 100vw;
   }
 `;
 
-// const MobileSvg = styled.img`
-//   max-width: 100%;
-//   width: calc(30% + 20vw);
-//   height: auto;
-//   z-index: 7;
-//   animation: ${move} 2.5s ease infinite;
-//   @media only Screen and (max-width: 48em) {
-//     align-self: flex-start;
-//     position: absolute;
-//     bottom: 0;
-//     width: calc(30% + 20vw);
-//     opacity: 0.5;
-//   }
-//   @media only Screen and (max-width: 40em) {
-//     display: none;
-//   }
-// `;
-
-// const Lb = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   align-items: flex-start;
-//   width: 50%;
-//   line-height: 1.5;
-//   color: var(--white);
-//   position: relative;
-//   z-index: 15;
-//   @media only Screen and (max-width: 48em) {
-//     width: 80%;
-//     text-align: center;
-//     align-items: center;
-//     justify-content: space-around;
-//     margin-top: calc(2.5rem + 2.5vw);
-//     filter: drop-shadow(2px 4px 6px black);
-//   }
-//   @media only Screen and (max-width: 40em) {
-//     filter: none;
-//   }
-// `;
-
-// const Topic = styled.span`
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   background-color: var(--nav);
-//   color: var(--white);
-//   font-weight: 700;
-//   font-size: calc(0.4rem + 0.4vw);
-//   padding: 0.5rem 1rem;
-//   border-radius: 20px;
-// `;
-
-// const Circle = styled.span`
-//   display: inline-block;
-//   width: 1rem;
-//   height: 1rem;
-//   border-radius: 50%;
-//   background-color: var(--purple);
-//   margin-right: 0.5rem;
-// `;
-
-// const Title = styled.h1`
-//   font-size: calc(2rem + 1vw);
-//   line-height: 1.2;
-//   padding: 0.5rem 0;
-// `;
-
-// const SubText = styled.h5`
-//   font-size: calc(0.5rem + 0.5vw);
-//   color: var(--nav2);
-// `;
-
-// const CTA = styled.button`
-//   background-color: var(--white);
-//   color: #0a0b10;
-//   padding: 0.5rem 1rem;
-//   margin-top: 1rem;
-//   border-radius: 20px;
-//   cursor: pointer;
-//   font-size: calc(0.5rem + 0.5vw);
-//   font-weight: 700;
-//   display: flex;
-//   align-items: center;
-//   transition: transform 0.2s;
-
-//   img {
-//     width: 1.5rem;
-//   }
-//   @media only screen and (max-width: 48em) {
-//     padding: 0.2rem 1rem;
-//   }
-//   &:hover {
-//     transform: scale(1.1);
-//   }
-//   &:active {
-//     transform: scale(0.9);
-//   }
-// `;
-
 const HeroSection = () => {
+  useEffect(() => {
+    new Vivus("vistaarOutline", {
+      duration: 200,
+      type: "delayed",
+      file: vistaarOutline,
+      pathTimingFunction: Vivus.EASE,
+    });
+  }, []);
+
   return (
     <HomeSection
       id="home"
-      style={{
-        backgroundImage: `url(${bgImage})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover", // Adjusts image to fit the section
-        backgroundPosition: "center", // Centers the image
-      }}
+      className="home-section"
+      // style={{
+      //   backgroundImage: `url(${bgImage})`,
+      //   backgroundRepeat: "no-repeat",
+      //   backgroundSize: "cover",
+      //   backgroundPosition: "center",
+      // }}
     >
+      <div className="two-logos">
+      <img
+        data-scroll
+        data-scroll-speed="1.5"
+        className="landing-logo"
+        alt="the main vistaar logo"
+        src={vistaar}
+        style={{width: "50vw", height: "auto", position: "relative"}}
+      />
 
-        <img
-            data-scroll
-            data-scroll-speed="1.5"
-            class="landing-logo"
-            alt="the main vistaar logo"
-            src={vistaar}
-          />
-  
-
-      
-
-
-      <MainContent id="home">
-
-        <div style={{ position: "relative", display: "inline-block" }}>
-
-
-
-          {/* CSS Animation */}
-          <style>
-            {`
-      @keyframes breath {
-        0% { transform: translate(-50%, -50%) scale(1); }
-        50% { transform: translate(-50%, -50%) scale(1.2); }
-        100% { transform: translate(-50%, -50%) scale(1); }
-      }
-    `}
-          </style>
-        </div>
-
-
-
+      <MainContent>
+        {/* SVG Animation Placeholder */}
+        <div id="vistaarOutline" className="landing-logo-outline" style={{ width: "50vw", height: "auto", position: "relative" }}></div>
       </MainContent>
+      </div>
     </HomeSection>
   );
 };
-
 
 export default HeroSection;
