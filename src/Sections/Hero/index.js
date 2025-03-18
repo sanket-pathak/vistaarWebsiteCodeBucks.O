ac4e6c7886082db67fba43f8617b58831a7a177a
Sanket Pathak <sanketpathak2005@gmail.com>
2025-03-19 02:39:15 +0530

    Vistaar zoom scroll animation added

--- src/Sections/Hero/index.js
+++ src/Sections/Hero/index.js
@@ -1,10 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import styled, { keyframes } from "styled-components";
 import bgImage from "../../assets/landingBG4.jpg";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import vistaar from "../../assets/Vistaar.svg";
 import vistaarOutline from "../../assets/vistaarOutline.svg";
 import Vivus from "vivus";
 import "./index.css";
+import { gsap } from "gsap";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const HomeSection = styled.section`
   width: 100vw;
@@ -37,12 +41,33 @@ const MainContent = styled.div`
 `;
 
 const HeroSection = () => {
+
+  const logoRef = useRef(null);
+
   useEffect(() => {
     new Vivus("vistaarOutline", {
       duration: 200,
       type: "delayed",
       file: vistaarOutline,
       pathTimingFunction: Vivus.EASE,
+    });
+  }, []);
+
+
+  useEffect(() => {
+    gsap.to(logoRef.current, {
+      scale: 8, // Adjust for smooth zoom (avoid extreme values)
+      duration: 2,
+      opacity: 0,
+      transformOrigin: "center center", // Ensures scaling from center
+      scrollTrigger: {
+        trigger: logoRef.current,
+        pin: true,
+        // markers: true,
+        start: "top top",
+        end: "top+=300px",// Adjust end position
+        scrub: true,
+      },
     });
   }, []);
 
@@ -59,12 +84,16 @@ const HeroSection = () => {
     >
       <div className="two-logos">
       <img
-        data-scroll
-        data-scroll-speed="1.5"
+        ref={logoRef}
+        // data-scroll
+        // data-scroll-speed="1.5"
         className="landing-logo"
         alt="the main vistaar logo"
         src={vistaar}
-        style={{width: "50vw", height: "auto", position: "relative"}}
+        style={{
+          width: "50vw", // 🔹 Reduced initial size (was 50vw)
+          height: "auto",
+        }}
       />
 
       <MainContent>
